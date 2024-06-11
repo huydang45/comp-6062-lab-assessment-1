afaaab8937fdfeec161fa58c0429d7179ad76231
Dang Huy <56798793+xuanhuy-primelabo@users.noreply.github.com>
2024-06-11 18:55:10 -0400

    add comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,10 @@
+// Create the variables to contain my information
 const studentName = "Dang Nguyen Xuan Huy";
 const studentNumber = 1161059;
 const combineInfo = `${studentName} - ${studentNumber}`;
 console.log(combineInfo);
-
+// Change content of h1 heading
 const headerContent = document.querySelector("h1");
 headerContent.innerHTML = combineInfo;
-
+// Adding header class
 headerContent.classList.add("heading1");
